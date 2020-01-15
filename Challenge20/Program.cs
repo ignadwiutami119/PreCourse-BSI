25174b9960437fe0bb9813b822d0f6e98861ef67
Igna Dwi Utami <Igna.Utami@bsi.co.id>
2020-01-15 08:38:49 +0700

    last commit

--- Challenge20/Program.cs
+++ Challenge20/Program.cs
@@ -1,34 +1,35 @@
 using System;
 
-namespace PreCourse
-{
+namespace PreCourse {
 
-    public class Item{
-        
-		public String name;
-		public double price;
-		public bool onSale;
-		
-		public double print(double price){
-			if(onSale)
-				price = price - price * 0.2;
-			
-			Console.Write("{0} ({1})", name, price);
-            return price;
-		}
-	}
+    public class Item {
+        public static String name;
+        public static double price;
+        public static bool onSale;
 
-    class Program
-    {
-        static void Main(string[] args)
-        {
+        public static void print () {
+            if (onSale)
+                price = price - price * 0.2;
+
+            Console.Write ("{0} ({1})", name, price);
+        }
+
+        public static string testPrint () {
+            if (onSale)
+                price = price - price * 0.2;
+
+            return $"{name} (Rp {price})";
+        }
+
+    }
+
+    public class Program1 {
+        static void Main (string[] args) {
             // Make this code works by create new class, method, and property.
-            Item item = new Item();
-            item.name = "SilverQueen";
-            item.price = 15000;
-            item.onSale = true; // If true the price will drop 20%
-
-            item.print(15000); // Output: "Indomie Goreng (Rp2800)"
+            Item.name = "Indomie Goreng";
+            Item.price = 3500;
+            Item.onSale = true; // If true the price will drop 20%
+            Item.print (); // Output: "Indomie Goreng (Rp2800)"
         }
     }
 }
--- My_pre_test/UnitTest1.cs
+++ My_pre_test/UnitTest1.cs
@@ -1,230 +1,192 @@
 using System;
+using System.Collections;
+using System.Collections.Generic;
 using PreCourse;
 using Xunit;
-using System.Collections.Generic;
-using System.Collections;
 
-namespace My_pre_test
-{
-    public class cek1
-    {
+namespace My_pre_test {
+    public class cek1 {
         [Fact]
-        public void Test1()
-        {
-            Assert.Equal("igna", Program.callname("igna"));
+        public void Test1 () {
+            Assert.Equal ("igna", Program.callname ("igna"));
         }
     }
 
-    public class cek2
-    {
+    public class cek2 {
         [Fact]
-        public void Test2()
-        {
-            Assert.NotEqual(30, year.gapYear("1997/03/19"));
+        public void Test2 () {
+            Assert.NotEqual (30, year.gapYear ("1997/03/19"));
         }
     }
 
-    public class cek3
-    {
+    public class cek3 {
         [Fact]
-        public void Test3()
-        {
-            Assert.Equal(5, panjangKata.CountString("abcde"));
+        public void Test3 () {
+            Assert.Equal (5, panjangKata.CountString ("abcde"));
         }
     }
 
-    public class cek4
-    {
+    public class cek4 {
         [Fact]
-        public void Test4()
-        {
-            var censoredWords = new ArrayList();
-            censoredWords.Add("imperdiet");
-            censoredWords.Add("dolor");
-            censoredWords.Add("duo");
-            Assert.Equal("Lorem ipsum ***** sit amet, ********* vituperata *** in, nonumy.", Subs.Cek("Lorem ipsum dolor sit amet, imperdiet vituperata duo in, nonumy.", censoredWords));
+        public void Test4 () {
+            var censoredWords = new ArrayList ();
+            censoredWords.Add ("imperdiet");
+            censoredWords.Add ("dolor");
+            censoredWords.Add ("duo");
+            Assert.Equal ("Lorem ipsum ***** sit amet, ********* vituperata *** in, nonumy.", Subs.Cek ("Lorem ipsum dolor sit amet, imperdiet vituperata duo in, nonumy.", censoredWords));
         }
     }
 
-    public class cek5
-    {
+    public class cek5 {
         [Fact]
-        public void Test5()
-        {
-            Assert.True(EvenOdd.isOdd(5));
-            Assert.True(EvenOdd.isEven(6));
+        public void Test5 () {
+            Assert.True (EvenOdd.isOdd (5));
+            Assert.True (EvenOdd.isEven (6));
         }
     }
 }
 
-public class cek6
-{
+public class cek6 {
     [Fact]
-    public void Test6()
-    {
-        Assert.Equal('A', Grade.Grading(95));
-        Assert.Equal('B', Grade.Grading(85));
-        Assert.Equal('C', Grade.Grading(75));
-        Assert.Equal('D', Grade.Grading(65));
-        Assert.Equal('E', Grade.Grading(55));
+    public void Test6 () {
+        Assert.Equal ('A', Grade.Grading (95));
+        Assert.Equal ('B', Grade.Grading (85));
+        Assert.Equal ('C', Grade.Grading (75));
+        Assert.Equal ('D', Grade.Grading (65));
+        Assert.Equal ('E', Grade.Grading (55));
     }
 }
 
-public class cek7
-{
+public class cek7 {
     [Fact]
-    public void Test7()
-    {
-        Assert.Equal(212, Temperature.CelciusToFahrenheit(100));
-        Assert.Equal(373.15, Temperature.CelciusToKelvin(100));
-        Assert.Equal(-173.15, Temperature.KelvinToCelcius(100));
-        Assert.Equal(-279.67, Temperature.KelvinToFahrenheit(100));
-        Assert.Equal(37.78, Temperature.FahrenheitToCelcius(100));
-        Assert.Equal(310.928, Temperature.FahrenheitToKelvin(100));
+    public void Test7 () {
+        Assert.Equal (212, Temperature.CelciusToFahrenheit (100));
+        Assert.Equal (373.15, Temperature.CelciusToKelvin (100));
+        Assert.Equal (-173.15, Temperature.KelvinToCelcius (100));
+        Assert.Equal (-279.67, Temperature.KelvinToFahrenheit (100));
+        Assert.Equal (37.78, Temperature.FahrenheitToCelcius (100));
+        Assert.Equal (310.928, Temperature.FahrenheitToKelvin (100));
     }
 }
 
-public class cek8
-{
+public class cek8 {
     [Fact]
-    public void Test8()
-    {
-        Assert.True(LeapYear.IsLeapYear(2024));
+    public void Test8 () {
+        Assert.True (LeapYear.IsLeapYear (2024));
     }
 }
 
-public class cek9
-{
+public class cek9 {
     [Fact]
-    public void Test9()
-    {
-        var jakarta = Tuple.Create("Jakarta", 7);
-        var bali = Tuple.Create("Bali", 8);
-        var london = Tuple.Create("London", 0);
-        var cairo = Tuple.Create("Cairo", 2);
-        var denver = Tuple.Create("Denver", -6);
-        var chicago = Tuple.Create("Chicago", -5);
-        Assert.Equal(7, Timezone.TimezoneDiff(jakarta, london));
+    public void Test9 () {
+        var jakarta = Tuple.Create ("Jakarta", 7);
+        var bali = Tuple.Create ("Bali", 8);
+        var london = Tuple.Create ("London", 0);
+        var cairo = Tuple.Create ("Cairo", 2);
+        var denver = Tuple.Create ("Denver", -6);
+        var chicago = Tuple.Create ("Chicago", -5);
+        Assert.Equal (7, Timezone.TimezoneDiff (jakarta, london));
     }
 }
 
-public class cek10
-{
+public class cek10 {
     [Fact]
-    public void Test10()
-    {
-        Assert.Equal(20, Calculator.Sum(10, 10));
-        Assert.Equal(100, Calculator.Multiply(10, 10));
-        Assert.Equal(0, Calculator.Substract(10, 10));
-        Assert.Equal(1, Calculator.Divide(10, 10));
+    public void Test10 () {
+        Assert.Equal (20, Calculator.Sum (10, 10));
+        Assert.Equal (100, Calculator.Multiply (10, 10));
+        Assert.Equal (0, Calculator.Substract (10, 10));
+        Assert.Equal (1, Calculator.Divide (10, 10));
     }
 }
 
-
-public class cek11
-{
+public class cek11 {
     [Fact]
-    public void Test11()
-    {
-        Assert.Equal(3, CountWords.Count("berlian sistem informasi"));
+    public void Test11 () {
+        Assert.Equal (3, CountWords.Count ("berlian sistem informasi"));
     }
 }
 
-public class cek12
-{
+public class cek12 {
     [Fact]
-    public void Test12()
-    {
-        Assert.Equal("ABCDEFG", Uppercase.cekUpper("abcdefg"));
+    public void Test12 () {
+        Assert.Equal ("ABCDEFG", Uppercase.cekUpper ("abcdefg"));
     }
 }
 
-public class cek13
-{
-    int[] num = {1,2,3,4,5};
+public class cek13 {
+    int[] num = { 1, 2, 3, 4, 5 };
     [Fact]
-    public void Test13()
-    {
-        Assert.Equal(15, Sum.jumlah(num));
+    public void Test13 () {
+        Assert.Equal (15, Sum.jumlah (num));
     }
 }
 
-
-public class cek14
-{
+public class cek14 {
     [Fact]
-    public void Test14()
-    {
-        Assert.True(Palindrom.IsPalindrome("kasur rusak"));
+    public void Test14 () {
+        Assert.True (Palindrom.IsPalindrome ("kasur rusak"));
     }
 }
 
-public class cek15
-{
+public class cek15 {
     [Fact]
-    public void Test15()
-    {
-        Assert.Equal("bookkoob", MirrorClass.Mirror("book"));
+    public void Test15 () {
+        Assert.Equal ("bookkoob", MirrorClass.Mirror ("book"));
     }
 }
 
-public class cek16
-{
+public class cek16 {
     [Fact]
-    public void Test16()
-    {
+    public void Test16 () {
         string[] fruits = {
-                "Jeruk",
-                "Apel",
-                "Anggur",
-                "Pepaya",
-                "Pisang",
-                "Kiwi",
-                "Markisa",
-            };
-            Assert.Equal(5, FruitFinder.IndexFinder(fruits, "Kiwi"));
+            "Jeruk",
+            "Apel",
+            "Anggur",
+            "Pepaya",
+            "Pisang",
+            "Kiwi",
+            "Markisa",
+        };
+        Assert.Equal (5, FruitFinder.IndexFinder (fruits, "Kiwi"));
     }
 }
 
-public class cek17
-{
+public class cek17 {
     [Fact]
-    public void Test17()
-    {
-        Assert.Equal("Baz", foobarbaz.value(100));
-        Assert.Equal("Foo", foobarbaz.value(25));
-        Assert.Equal("Bar", foobarbaz.value(40));
+    public void Test17 () {
+        Assert.Equal ("Baz", foobarbaz.value (100));
+        Assert.Equal ("Foo", foobarbaz.value (25));
+        Assert.Equal ("Bar", foobarbaz.value (40));
     }
 }
 
-public class cek18
-{
+public class cek18 {
     int[] numbers = { 2, 4, 54, 12, -65, 19, 40, 92, 88, 330, -4, 54 };
     [Fact]
-    public void Test18()
-    {
-        Assert.Equal(330, MinMaxAvg.FindMax(numbers));
-        Assert.Equal(-65, MinMaxAvg.FindMin(numbers));
-        Assert.Equal(52, MinMaxAvg.FindAverage(numbers));
+    public void Test18 () {
+        Assert.Equal (330, MinMaxAvg.FindMax (numbers));
+        Assert.Equal (-65, MinMaxAvg.FindMin (numbers));
+        Assert.Equal (52, MinMaxAvg.FindAverage (numbers));
     }
 }
 
-public class cek19
-{
-     int[] number = { 2, 4, 54, 12, -65, 19, 40, 92, 88, 330, -4, 56 };
-     int[] expected = {-65, -4, 2, 4, 12, 19, 40, 54, 56, 88, 92, 330};
-     [Fact]
-    public void Test19()
-    {
-        Assert.Equal(expected, Sorter.Sort(number));
+public class cek19 {
+    int[] number = { 2, 4, 54, 12, -65, 19, 40, 92, 88, 330, -4, 56 };
+    int[] expected = {-65, -4, 2, 4, 12, 19, 40, 54, 56, 88, 92, 330 };
+    [Fact]
+    public void Test19 () {
+        Assert.Equal (expected, Sorter.Sort (number));
     }
 }
 
-// public class cek20
-// {
-//      [Fact]
-//     public void Test20()
-//     {
-//         Assert.Equal(10000, Item.print(15000));
-//     }
-// }
+public class cek20 {
+
+    [Fact]
+    public void Test20 () {
+        string expected = "Silverqueen (Rp 12000)";
+        Item.name = "Silverqueen";
+        Item.price = 15000;
+        Item.onSale = true;
+        Assert.Equal (expected, Item.testPrint ());
+    }
+}
